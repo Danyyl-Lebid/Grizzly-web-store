26894e4e7b573ff4349911294ad4b1fff256e2ca
Белозерцев Артур <Turabot@i.ua>
2021-06-12 14:18:13 +0300

    Create properties test

--- src/main/java/com/github/grizzly/entity/User.java
+++ src/main/java/com/github/grizzly/entity/User.java
@@ -1,4 +1,14 @@
 package com.github.grizzly.entity;
 
 public class User {
+
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phone;
+
+//    private UserRole role;
 }
