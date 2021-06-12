95f830afa55d69aef494800c00a32d45eb307d85
Aleks <alexdodul3@gmail.com>
2021-06-12 12:35:08 +0300

    create Product.java

---
+++ src/main/java/com/github/grizzly/entity/Product.java
@@ -0,0 +1,34 @@
+package com.github.grizzly.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "description")
+    private Float price;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    public Product(String name, String description, Float price, int quantity) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
