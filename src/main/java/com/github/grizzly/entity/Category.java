7ad75f20051d4287f3ee60793b2c78dd32a64b62
Aleks <alexdodul3@gmail.com>
2021-06-13 12:32:47 +0300

    fix Category.java

--- src/main/java/com/github/grizzly/entity/Category.java
+++ src/main/java/com/github/grizzly/entity/Category.java
@@ -1,12 +1,28 @@
 package com.github.grizzly.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "category")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "description")
     private String description;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "category", cascade = CascadeType.ALL)
+    private Set<Product> products = new HashSet<>();
 
     public Category(String name, String description) {
         this.name = name;
