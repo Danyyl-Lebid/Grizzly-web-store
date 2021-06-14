e78f5c92d8f32f81ec19bd7a176249814eab00c6
Aleks <alexdodul3@gmail.com>
2021-06-14 11:22:10 +0300

    added CategoryRepository

---
+++ src/main/java/com/github/grizzly/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.github.grizzly.repository;
+
+import com.github.grizzly.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Category findCategoryByName(String name);
+
+}
