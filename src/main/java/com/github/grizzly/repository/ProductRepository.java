51ef60fbd3f19975b10cdd3b2b73c2ecf746cd0f
Aleks <alexdodul3@gmail.com>
2021-06-14 11:21:15 +0300

    added ProductRepository

---
+++ src/main/java/com/github/grizzly/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.github.grizzly.repository;
+
+import com.github.grizzly.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Product findById(long id);
+
+    Product findByName(String name);
+
+    List<Product> findAllByOrderById();
+
+    List<Product> findAllByCategoryId(long categoryId);
+
+}
