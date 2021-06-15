015d1daa83103a940e1461cea6e8dad49833957b
Danyyl-Lebid <64252230+Danyyl-Lebid@users.noreply.github.com>
2021-06-15 12:13:39 +0300

    NotNull removed

--- src/main/java/com/github/grizzly/entity/Category.java
+++ src/main/java/com/github/grizzly/entity/Category.java
@@ -1,7 +1,9 @@
 package com.github.grizzly.entity;
 
-import com.sun.istack.NotNull;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.HashSet;
