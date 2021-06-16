87ec6d78236925df8fc2c68851ce181ba3b643a2
Danyyl-Lebid <64252230+Danyyl-Lebid@users.noreply.github.com>
2021-06-16 23:33:03 +0300

    Initial commit

---
+++ src/test/resources/com/github/grizzly/repository/roles-data.sql
@@ -0,0 +1,4 @@
+insert into roles (id, role) values (null, 'GUEST');
+insert into roles (id, role) values (null, 'USER');
+insert into roles (id, role) values (null, 'MANAGER');
+insert into roles (id, role) values (null, 'ADMIN');
