47d33dca34f1609064f3c6b222764ec8e94ba986
Aleks <alexdodul3@gmail.com>
2021-06-18 02:30:06 +0300

    create two file sql for test ProductRepository

---
+++ src/test/java/com/github/grizzly/repository/product-data.sql
@@ -0,0 +1,4 @@
+insert into public.products (id, name, description, price, quantity, category_id) values (1, 'Milk-1', 'Test_description', 19.12, 10, 1);
+insert into public.products (id, name, description, price, quantity, category_id) values (2, 'Milk-2', 'Test_description', 19.12, 10, 1);
+insert into public.products (id, name, description, price, quantity, category_id) values (3, 'Milk-3', 'Test_description', 19.12, 10, 1);
+insert into public.products (id, name, description, price, quantity, category_id) values (4, 'Milk-4', 'Test_description', 19.12, 10, 1);
