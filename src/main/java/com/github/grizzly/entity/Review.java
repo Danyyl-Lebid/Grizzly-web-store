7479340f13b8481e28ae263b5ece0c6d7c4cb709
Bamsteh <andrey20111998@gmail.com>
2021-06-18 01:24:49 +0300

    review entity and repo (empty)

--- src/main/java/com/github/grizzly/entity/Review.java
+++ src/main/java/com/github/grizzly/entity/Review.java
@@ -1,14 +1,18 @@
 package com.github.grizzly.entity;
 
+import com.sun.istack.NotNull;
 import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
+@Data
+@Entity
+@Table(name = "review")
+@NoArgsConstructor
 public class Review {
 
     @Id
@@ -16,16 +20,25 @@ public class Review {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotNull
     @Column(name = "user_id", nullable = false)
     private long idReviewer;
 
     @Column(name = "review", columnDefinition = "VARCHAR(256)")
     private String review;
 
+    @NotNull
     @Column(name = "created_date", nullable = false)
     private Date createdAt;
 
-    @Column(name = "name", nullable = false)
+    @NotNull
+    @Column(name = "rate", nullable = false)
     private int rate;
 
+    public Review(long idReviewer, String review, Date createdAt, int rate) {
+        this.idReviewer = idReviewer;
+        this.review = review;
+        this.createdAt = createdAt;
+        this.rate = rate;
+    }
 }
---
+++ src/main/java/com/github/grizzly/repository/ReviewRepository.java
@@ -0,0 +1,7 @@
+package com.github.grizzly.repository;
+
+public class ReviewRepository {
+
+
+
+}
