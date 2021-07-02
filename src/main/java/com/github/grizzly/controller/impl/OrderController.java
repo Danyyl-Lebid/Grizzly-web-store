e542ce1f799cfcec833c3e747c4cc3babde0cee8
Vita Redko <rvitargo@gmail.com>
2021-07-02 12:53:23 +0300

    changed order controller

--- src/main/java/com/github/grizzly/controller/impl/OrderController.java
+++ src/main/java/com/github/grizzly/controller/impl/OrderController.java
@@ -11,6 +11,8 @@ import com.github.grizzly.entity.User;
 import com.github.grizzly.enums.Status;
 import com.github.grizzly.exceptions.EntityNotFoundException;
 import com.github.grizzly.repository.ProductRepository;
+import com.github.grizzly.security.CustomUserDetails;
+import com.github.grizzly.security.CustomUserDetailsService;
 import com.github.grizzly.security.jwt.JwtProvider;
 import com.github.grizzly.service.IOrderItemService;
 import com.github.grizzly.service.IOrderService;
@@ -25,11 +27,14 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.Collection;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -54,49 +59,70 @@ public class OrderController implements IOrderController {
     private final ProductRepository productRepository;
 
     @Autowired
-    JwtProvider jwtProvider;
+    private JwtProvider jwtProvider;
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @Override
-    public ResponseEntity<List<OrderDto>> findAll() {
-        return new ResponseEntity<>(this.orderService.readAll(), HttpStatus.OK);
-    }
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @Override
-    public ResponseEntity<List<OrderDto>> findAllByStatus(Status status) {
-        return new ResponseEntity<>(this.orderService.readAllOrdersByStatus(status), HttpStatus.OK);
-    }
-
-//    Need something like that @PreAuthorize("hasRole('ROLE_ADMIN') or #User.getId == #userId")
-    @Override
-    public ResponseEntity<List<OrderDto>> findAllOrderByUserId(long userId, String jwtToken) {
-        User user = this.userService.findById(userId);
-        return new ResponseEntity<>(this.orderService.readAllOrderByUser(user), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public List<OrderDto> findAll() {
+        return this.orderService.readAll()
+                .stream()
+                .map(OrderTransferObject::fromOrder)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public ResponseEntity<OrderDto> findOrderById(long userId, long orderId, String jwtToken) {
+    @ResponseStatus(HttpStatus.OK)
+    public List<OrderDto> findAllByStatus(Status status) {
+        return this.orderService.readAllOrdersByStatus(status)
+                .stream()
+                .map(OrderTransferObject::fromOrder)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @ResponseStatus(HttpStatus.OK)
+    public List<OrderDto> findAllOrderByUserId(String jwtToken) {
+        String login = jwtProvider.getLoginFromToken(jwtToken);
+        User user = this.userService.findByLogin(login);
+        return this.orderService.readAllOrderByUser(user)
+                .stream()
+                .map(OrderTransferObject::fromOrder)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @ResponseStatus(HttpStatus.OK)
+    public OrderDto findOrderById(long orderId) {
         Order order = this.orderService.findOrderById(orderId).orElseThrow(EntityNotFoundException::new);
-        return new ResponseEntity<>(OrderTransferObject.fromOrder(order), HttpStatus.OK);
+        return OrderTransferObject.fromOrder(order);
     }
 
     @Override
-    public ResponseEntity<List<OrderDto>> findAllOrderByUserIdAndByStatusCreateDateDesc(long userId, Status status, String jwtToken) {
-        User user = this.userService.findById(userId);
-        return new ResponseEntity<>(this.orderService.readAllOrderByUserAndByStatus(user, status), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public List<OrderDto> findAllOrderByUserIdAndByStatusCreateDateDesc(Status status, String jwtToken) {
+        String login = jwtProvider.getLoginFromToken(jwtToken);
+        User user = this.userService.findByLogin(login);
+        return this.orderService.readAllOrderByUserAndByStatus(user, status)
+                .stream()
+                .map(OrderTransferObject::fromOrder)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public ResponseEntity<OrderDto> save(long userId, String jwtToken, DistributeOrderDto payload) {
-        User user = this.userService.findById(userId);
+    @ResponseStatus(HttpStatus.CREATED)
+    public OrderDto save(String jwtToken, DistributeOrderDto payload) {
+        String login = jwtProvider.getLoginFromToken(jwtToken);
+        User user = this.userService.findByLogin(login);
         Order order = this.orderService.create(user);
         List<OrderItem> orderItems = payload.getOrderItems()
                 .stream()
                 .map(orderItemsDto -> this.orderItemService.create(orderItemsDto, order))
                 .collect(Collectors.toList());
         order.setOrderItems(orderItems);
-        return new ResponseEntity<>(OrderTransferObject.fromOrder(order), HttpStatus.CREATED);
+        return OrderTransferObject.fromOrder(order);
     }
 
     @Override
