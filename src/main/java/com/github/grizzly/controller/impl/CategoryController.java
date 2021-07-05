bbc52656dfe6d44b9c0664a2fe3180eadcab2c3e
Bamsteh <andrey20111998@gmail.com>
2021-07-05 03:06:50 +0300

    CategoryController and Transfer + dto

---
+++ src/main/java/com/github/grizzly/controller/impl/CategoryController.java
@@ -0,0 +1,46 @@
+package com.github.grizzly.controller.impl;
+
+import com.github.grizzly.controller.ICategoryController;
+import com.github.grizzly.dto.CategoryDto;
+import com.github.grizzly.repository.CategoryRepository;
+import com.github.grizzly.utils.CategoryTransferObject;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.github.grizzly.utils.CategoryTransferObject.toCategory;
+import static com.github.grizzly.utils.CategoryTransferObject.fromCategory;
+
+@RestController
+@RequestMapping(path = "/category")
+@RequiredArgsConstructor
+public class CategoryController implements ICategoryController {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public List<CategoryDto> findAll() {
+        return this.categoryRepository.findAll()
+                .stream()
+                .map(CategoryTransferObject::fromCategory)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CategoryDto update(CategoryDto category) {
+        return fromCategory(this.categoryRepository.save(toCategory(category)));
+    }
+
+    @Override
+    public CategoryDto save(CategoryDto category) {
+        return fromCategory(this.categoryRepository.save(toCategory(category)));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        this.categoryRepository.deleteById(id);
+    }
+}
