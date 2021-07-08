2d30aaa69f1faf92cbfa86d28c89ac4b487c13e4
Aleks <alexdodul3@gmail.com>
2021-07-08 11:22:06 +0300

    copy OrderService

--- src/main/java/com/github/grizzly/service/impl/OrderService.java
+++ src/main/java/com/github/grizzly/service/impl/OrderService.java
@@ -1,21 +1,16 @@
 package com.github.grizzly.service.impl;
 
-import com.github.grizzly.dto.order.OrderDto;
 import com.github.grizzly.entity.ActiveState;
 import com.github.grizzly.entity.Order;
 import com.github.grizzly.entity.User;
 import com.github.grizzly.enums.Status;
 import com.github.grizzly.repository.OrderRepository;
 import com.github.grizzly.service.IOrderService;
-import com.github.grizzly.utils.OrderTransferObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -54,7 +49,7 @@ public class OrderService implements IOrderService {
     }
 
     @Override
-    public Optional <Order> findOrderById(long id) {
+    public Optional<Order> findOrderById(long id) {
         return this.orderRepository.findById(id);
     }
 
@@ -72,6 +67,6 @@ public class OrderService implements IOrderService {
 
     @Override
     public void deleteById(Long id) {
-            this.orderRepository.deleteById(id);
+        this.orderRepository.deleteById(id);
     }
 }
